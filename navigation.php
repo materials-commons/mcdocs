e794d2be16b2997aa64276e28e10263154f9bd9e
gtarcea <glenn.tarcea@gmail.com>
2019-12-05 14:29:47 -0500

    Initial template and some customization for documentation site

---
+++ navigation.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'Getting Started' => [
+        'url' => 'docs/getting-started',
+        'children' => [
+            'Customizing Your Site' => 'docs/customizing-your-site',
+            'Navigation' => 'docs/navigation',
+            'Algolia DocSearch' => 'docs/algolia-docsearch',
+            'Custom 404 Page' => 'docs/custom-404-page',
+            'Spreadsheets' => 'docs/spreadsheets'
+        ],
+    ],
+    'Jigsaw Docs' => 'https://jigsaw.tighten.co/docs/installation',
+];
